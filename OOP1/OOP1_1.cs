2719a73451cbd65cc97a1fefb9fa755715581e14
sabibulaev25@gmail.com <sabibulaev25@gmail.com>
2023-02-28 14:34:43 +0300

    Добавьте файлы проекта.

---
+++ OOP1/OOP1_1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+Pet cat = new Pet("Lypus", "cat");
+Animal home_animal = cat;   // преобразование от Pet к Animal
+Console.WriteLine(home_animal.Name);
+namespace OOP1_1
+{
+    class OOP1_1
+    {
+        static void Main(string[] args)
+        {
+            //Неявное преобразование простых типов
+            float Pi = 3.14F;
+            double P = Pi; //float в double
+            int t = 3;
+            P = t; //int в double
+            char i = '5';
+            t = i; //char в int
+        }
+    }
+}
+
+class Animal
+{
+    public string Name { get; set; }
+    public Animal(string name)
+    {
+        Name = name;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Animal {Name}");
+    }
+}
+
+//Наследник класса Animal
+class Pet : Animal
+{
+    public string Type { get; set; }
+    public Pet(string name, string pet) : base(name)
+    {
+        Type = pet;
+    }
+}
