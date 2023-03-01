fb70053755813ee68c57deb66a86632eac3c7ce7
WhiteSun13 <99079499+WhiteSun13@users.noreply.github.com>
2023-03-01 03:15:27 +0300

    OOP1 3-4

---
+++ OOP1_3/OOP1_3.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace OOP1_3
+{
+    class OOP1_3
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                long a = 555000000000;
+                long b = 700000000000;
+                int c = checked((int)(a + b)); //В int не поместится такое огромное число
+                Console.WriteLine(c);
+            }
+            catch (OverflowException ex) //"Ловец" ошибки
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ OOP1_4/OOP1_4.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+Animal animal1 = new Animal("dog");
+Pet? beaver = animal1 as Pet; //? указывает что переменная может хранить null или Pet.
+if (beaver == null) //Можно ли перевести animal к pet
+{
+Console.WriteLine("Преобразование провалено");
+}
+else
+{
+Console.WriteLine(beaver.Type);
+}
+//В обратную сторону
+Pet cat = new Pet("Morgana", "Cat");
+Animal? animal2 = cat as Animal; //Можно преобразовать переменную производного класса к базовому типу
+if (animal2 == null)
+{
+Console.WriteLine("Преобразование прошло неудачно");
+}
+else
+{
+Console.WriteLine(animal2.Name);
+}
+
+Animal animal3 = new Animal("Lion"); //Переменная базового типа
+if (animal3 is Pet pet) //Можно ли представить ее как переменную производного класса
+{
+Console.WriteLine(pet.Type);
+}
+else
+{
+Console.WriteLine("Преобразование не возможно"); //Не можно
+}
+
+class Animal
+{
+    public string Name { get; set; }
+    public Animal(string name)
+    {
+        Name = name;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Animal {Name}");
+    }
+}
+class Pet : Animal
+{
+    public string Type { get; set; }
+    public Pet(string name, string pet) : base(name)
+    {
+        Type = pet;
+    }
+}
+
+class WildAnimal : Animal
+{
+    public string Habitat { get; set; }
+    public WildAnimal(string name, string habitat) : base(name)
+    {
+        Habitat = habitat;
+    }
+}
