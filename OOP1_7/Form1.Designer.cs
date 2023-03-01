c7cd5db0981840c7af2ceff33f02917b0e94a169
WhiteSun13 <99079499+WhiteSun13@users.noreply.github.com>
2023-03-01 03:19:19 +0300

    OOP1 7

---
+++ OOP1_7/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿
+namespace OOP1_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Enter = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 47);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(125, 27);
+            this.textBox1.TabIndex = 0;
+            // 
+            // Enter
+            // 
+            this.Enter.Location = new System.Drawing.Point(164, 47);
+            this.Enter.Name = "Enter";
+            this.Enter.Size = new System.Drawing.Size(94, 29);
+            this.Enter.TabIndex = 1;
+            this.Enter.Text = "Ввод";
+            this.Enter.UseVisualStyleBackColor = true;
+            this.Enter.Click += new System.EventHandler(this.CheckText);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(271, 117);
+            this.Controls.Add(this.Enter);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button Enter;
+    }
+}
+
---
+++ OOP1_7/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace OOP1_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void CheckText(object sender, EventArgs e)
+        {
+            int test;
+            bool result = double.TryParse(textBox1.Text, out var number); //Преобразование ввода в число
+            if (result == true)
+            {
+                test = (int)number;
+                //Если введено было число, и при этом при переводе в int теряется дробная часть, то выводим
+                if (number - test != 0)
+                {
+                    MessageBox.Show($"Число: {number}");
+                }
+                else
+                    MessageBox.Show("Число не соответсвует установленному формату");
+            }
+            else
+                MessageBox.Show("Число не соответсвует установленному формату");
+        }
+    }
+}
