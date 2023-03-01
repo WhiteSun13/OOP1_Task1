b5e3f08d088d08bde96d4465c15ea2e3832f6851
WhiteSun13 <99079499+WhiteSun13@users.noreply.github.com>
2023-03-01 03:17:30 +0300

    OOP1 5-6

---
+++ OOP1_5/OOP1_5.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+Salary new_salary = new Salary {Rubles = 24130};
+int x = (int)new_salary; //Явно указываем тип
+Console.WriteLine($"Явный перевод: {x}");
+
+Salary prev_salary = x;
+Console.WriteLine($"Неявный перевод: {prev_salary.Rubles}");
+//Класс, хранящий размер зарплаты и хранит ее в рублях в свойстве Rubles
+class Salary
+{
+    public int Rubles {get; set;}
+    //Неявное преобразование, число в объект
+    public static implicit operator Salary(int x) //Salary - тип, во что преобразовываем
+    //В скобках - что преобразовываем
+    {
+        return new Salary {Rubles = x};
+    }
+    //Явное преобразование
+    //Позволяет получить число из объекта
+    public static explicit operator int(Salary salary)
+    {
+        return salary.Rubles;
+    }
+}
+
---
+++ OOP1_6/OOP1_6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace OOP1_6
+{
+    class OOP1_6
+    {
+        static void Main(string[] args)
+        {
+            int f = 50;
+            string conv = f.ToString(); //Число в строку
+            conv = "546";
+            f = int.Parse(conv); //Строку в число при помощи Parse
+            conv = Console.ReadLine();
+            bool result = int.TryParse(conv, out var number); //Преобразование строки в число TryParse
+            if (result == true)
+                Console.WriteLine($"Преобразование завершено. Число: {number}");
+            else
+                Console.WriteLine("Преобразование не удалось.");
+            //Преобразование числа в логический тип Convert
+            int tru = 2;
+            bool d = Convert.ToBoolean(tru);
+            Console.WriteLine(d);
+        }
+    }
+}
